d178a2eac6fbfef970847d78b0df6566064730dd
LordPikak <wpiecuch012@gmail.com>
2020-10-22 13:11:48 +0200

    Points and distances between them

--- Lab 3/main.cpp
+++ Lab 3/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <cmath>
 using namespace std;
 
 
@@ -7,13 +10,38 @@ struct Point
     double x;
     double y;
 };
+float Distance(Point A, Point B)
+{
+    double result;
+    result=sqrt(pow(A.x-B.x,2)+pow(A.y-B.y,2));
+    return result;
+}
+Point AssignPoint()
+{
+    Point temp;
+    temp.x=rand()%100;
+    temp.y=rand()%100;
+    return temp;
+}
 
 int main()
 {
-    Point mypoint;
-    mypoint.x=2;
-    mypoint.y=3.3;
-    cout<<mypoint.x<<endl;
-    cout<<mypoint.y<<endl;
+    double largdist=0;
+    Point Points[10];
+    for (int i=0; i<10; i++)
+    {
+        Points[i]=AssignPoint();
+        cout << Points[i].x<<" "<<Points[i].y<<endl;
+    }
+    for (int i=0; i<10; i++)
+    {
+
+        for (int j=0; j<10; j++)if(j!=i)
+        {
+            cout<<Distance(Points[i],Points[j])<<endl;
+            if (Distance(Points[i],Points[j])>largdist)largdist=Distance(Points[i],Points[j]);
+        }
+    }
+    cout <<largdist;
     return 0;
 }
