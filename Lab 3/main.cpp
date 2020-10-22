26dc39b4653083a4fe4807467a6053407fc85b77
LordPikak <wpiecuch012@gmail.com>
2020-10-22 12:14:21 +0200

    Bubble sort

--- Lab 3/main.cpp
+++ Lab 3/main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
-
 using namespace std;
 
 
 
 int main()
 {
-
+    int temp;
+    int numbahs[10]= {7, 3, 9, 2, 5, 1, 5, 20, 8, 4};
+    for (int j=10; j>0; j--)
+    {
+        for (int i=0; i<10; i++)
+            {
+                if (numbahs[i]<numbahs[i+1])
+                {
+                    temp=numbahs[i];
+                    numbahs[i]=numbahs[i+1];
+                    numbahs[i+1]=temp;
+                }
+                cout << numbahs[i];
+            }
+        cout << endl;
+    }
 
 
     return 0;
